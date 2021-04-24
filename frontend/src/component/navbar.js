f46001cb522b12b6d1e80e388f6162307b3ee9b9
Tarique <tariqueansari455@gmail.com>
2021-04-24 21:41:23 +0530

    initial react setup

---
+++ frontend/src/component/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function  Navbar() {
+  return (
+    <div className="container">
+        <div className="row">
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  );
+}
